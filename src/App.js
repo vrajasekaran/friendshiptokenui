d627a2a6b086d07a381baa3c4ed430cab8bdcaa8
vrajasekaran <vprajasekaran@gmail.com>
2018-02-27 18:08:29 -0500

    initial working version
    this includes the compiled contracts.json files from Contract project

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { drizzleConnect } from "drizzle-react";
+import { ContractData, ContractForm } from "drizzle-react-components";
+
+class App extends Component {
+  render() {
+    const { drizzleStatus, accounts } = this.props;
+
+    if (drizzleStatus.initialized) {
+      return (
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Friendship Token</h1>
+            <p>
+              <strong>Total Supply</strong>:{" "}
+              <ContractData
+                contract="FriendshipToken"
+                method="totalSupply"
+                methodArgs={[{ from: accounts[0] }]}
+              />{" "}
+              <ContractData
+                contract="FriendshipToken"
+                method="symbol"
+                hideIndicator
+              />
+            </p>
+            <p>
+              <strong>My Balance</strong>:{" "}
+              <ContractData
+                contract="FriendshipToken"
+                method="balanceOf"
+                methodArgs={[accounts[0]]}
+              />
+            </p>
+            <h3>Send Tokens</h3>
+          </header>
+          <div className="App-intro">
+            <ContractForm
+              contract="FriendshipToken"
+              method="transfer"
+              labels={["To Address", "Amount to Send"]}
+            />
+          </div>
+        </div>
+      );
+    }
+
+    return <div>Loading dapp...</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    accounts: state.accounts,
+    drizzleStatus: state.drizzleStatus,
+    FriendshipToken: state.contracts.FriendshipToken
+  };
+};
+
+const AppContainer = drizzleConnect(App, mapStateToProps);
+export default AppContainer;
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { DrizzleProvider } from 'drizzle-react'
+
+
+import FriendshipToken from './contracts/FriendshipToken.json'
+
+console.log(FriendshipToken);
+
+const options = {
+    web3: {
+        block: false,
+        fallback: {
+            type: "ws",
+            url:  "ws://127.0.0.1:7545"
+        }
+    },
+    contracts: [FriendshipToken],
+    events: {}
+};
+
+ReactDOM.render(
+<DrizzleProvider options={options} > 
+    <App/>
+</DrizzleProvider>,
+     document.getElementById('root'));
+registerServiceWorker();
